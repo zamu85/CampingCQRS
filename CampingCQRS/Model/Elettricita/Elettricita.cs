a010b46009efa09daa4741e8a3c5af858b555911
Riccardo Zamuner <riccardo85zamuner@gmail.com>
2023-09-05 11:23:41 +0200

    Model classe elettricita

---
+++ CampingCQRS/Model/Elettricita/Elettricita.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Model.Elettricita
+{
+    public class Elettricita
+    {
+        public double Potenza { get; set; }
+
+        public int IdElettricita { get; set; } = 0;
+    }
+}
