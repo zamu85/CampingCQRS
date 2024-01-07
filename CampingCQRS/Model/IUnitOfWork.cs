b3185464eabc8486fd3587787524ecff1957d556
Riccardo Zamuner <riccardo85zamuner@gmail.com>
2024-01-07 14:47:44 +0100

    Aggiunto unit of work repository pattern

--- CampingCQRS/Model/IUnitOfWork.cs
+++ CampingCQRS/Model/IUnitOfWork.cs
@@ -1,6 +1,12 @@
-﻿namespace Model;
+﻿using Model.Alloggio;
+using Model.Camera;
+using Model.Elettricita;
+
+namespace Model;
 
 public interface IUnitOfWork : IDisposable
 {
-
+    ICameraRepository CameraRepository { get; }
+    IAlloggioRepository AlloggioRepository { get; }
+    IElettricitaRepository ElettricitaRepository { get; }
 }
